c5571e9c4cde2440b77e92b60a194ad442efa653
blanca <978059629@qq.com>
2022-06-03 16:43:51 +0800

    Auto stash before rebase of "origin/main"

---
+++ component/search/Poster.tsx
@@ -0,0 +1,11 @@
+import css from './Poster.module.scss'
+const Poster: React.FC = () => {
+  return (
+    <div className={css.PosterContainer}>
+      <div>Search Dribbble</div>
+      <div>18,300,000+ images from thousands of inspirational designers</div>
+    </div>
+  )
+}
+
+export default Poster
